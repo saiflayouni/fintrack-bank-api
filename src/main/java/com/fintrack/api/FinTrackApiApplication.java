de331d0b69e341ccbe886cd6e503b8311ecc3a1a
saiflayouni <saiflayouni77@gmail.com>
2026-01-10 22:31:47 +0100

    chore: initial commit

---
+++ src/main/java/com/fintrack/api/FinTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.fintrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintrack/api/FinTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
